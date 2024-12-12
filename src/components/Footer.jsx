8a66e9f809fb28b9ab11d6dfabb1e9aaf637e63a
morjina <fmorjina111@gmail.com>
2024-12-12 22:12:51 +0600

    Fix: Build Issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,16 @@ import { AiFillInstagram } from "react-icons/ai";
 
 function Footer() {
   return (
-    <foooter className="mx-auto bg-[#F7F7F7] text-black dark:bg-gray-800 dark:text-white pt-20 mt-5 pb-5">
+    <footer className="mx-auto bg-[#F7F7F7] text-black dark:bg-gray-800 dark:text-white pt-20 mt-5  pb-5">
        <div className="section_contniner grid grid-col-1 sm:grid-cols-2 md:grid-cols-5 gap-8 ">
        {/* logo and Ditals  */}
           <div className="md:col-span-2">
-              <h2 className="text-2xl font-bold mb-4"><Link to="/">panto</Link></h2>
+              <h2 className="text-2xl font-bold mb-4 "><Link to="/">panto</Link></h2>
               <p className="md:mr-12">The advantage of hiring a workspace with us is that givees you comfortable service and all-around facilities.</p>
           </div>
           {/* Services div */}
           <div>
-              <h2 className="text-xl font-semibold mb-4">Services</h2>
+              <h2 className="text-xl font-semibold mb-4 text-primary">Services</h2>
               <ul className="space-y-2 ">
                  <li>
                      <Link to="/" className="hover:text-primary">Email Marketing</Link>
@@ -32,7 +32,7 @@ function Footer() {
           </div>
           {/* Furniture div */}
           <div>
-              <h2 className="text-xl font-semibold mb-4">Furniture</h2>
+              <h2 className="text-xl font-semibold mb-4 text-primary">Furniture</h2>
               <ul  className="space-y-2 ">
                  <li>
                      <Link to="/" className="hover:text-primary">Beds</Link>
@@ -47,16 +47,16 @@ function Footer() {
           </div>
           {/* Follow Us div */}
           <div  className="space-y-2 ">
-              <h2 className="text-xl font-semibold mb-4">Follow Us</h2>
+              <h2 className="text-xl font-semibold mb-4 text-primary">Follow Us</h2>
               <ul>
                  <li>
-                     <Link to="/" className="hover:text-primary flex items-center space-x-2"><FaFacebook />Facebook</Link>
+                     <Link to="/" className="hover:text-primary flex items-center gap-2 space-x-2"><FaFacebook />Facebook</Link>
                  </li>
                  <li>
-                     <Link to="/" className="hover:text-primary flex items-center space-x-2"><IoLogoTwitter /> Twitter</Link>
+                     <Link to="/" className="hover:text-primary flex items-center gap-2 space-x-2"><IoLogoTwitter /> Twitter</Link>
                  </li>
                  <li>
-                     <Link to="/" className="hover:text-primary flex space-x-2 items-center"><AiFillInstagram /> Instagram</Link>
+                     <Link to="/" className="hover:text-primary flex gap-2 space-x-2 items-center"><AiFillInstagram /> Instagram</Link>
                  </li>
               </ul>
           </div>
@@ -70,7 +70,7 @@ function Footer() {
               <p><Link to="/" className="hover:text-primary">Privacy Policy </Link></p>
           </div>
        </div>
-    </foooter>
+    </footer>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx';
-import Shop from './pages/shop/shop.jsx'
 import About from './pages/about/About.jsx'
 import Contact from './pages/contact/Contact.jsx'
+import Shop from './pages/shop/Shop.jsx'
 
 const router = createBrowserRouter([
   {
